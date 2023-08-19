fcad2117c6f959241723cbcccfa87480328eafdb
kotnala <ankitkotnala@live.in>
2023-08-19 22:19:03 +0530

    corrected

--- azurerm_private_endpoint/main.tf
+++ azurerm_private_endpoint/main.tf
@@ -10,14 +10,32 @@ resource "azurerm_private_endpoint" "this" {
     content{
         name = private_dns_zone_group.value["name"]
         private_dns_zone_ids = private_dns_zone_group.value["private_dns_zone_ids"]
-        private_connection_resource_alias = private_dns_zone_group.value["private_connection_resource_alias"]
-        private_connection_resource_id = private_dns_zone_group.value["private_connection_resource_id"]
-        subresource_names = private_dns_zone_group.value["subresource_names"]
+    }
+  }
+
+  dynamic "private_service_connection" {
+
+        for_each = var.private_service_connection
+    content{
+        name = private_service_connection.value["name"]
+        is_manual_connection = private_service_connection.value["is_manual_connection"]
+        private_connection_resource_alias = private_service_connection.value["private_connection_resource_alias"]
+        private_connection_resource_id = private_service_connection.value["private_connection_resource_id"]
+        subresource_names = private_service_connection.value["subresource_names"]
+        request_message = private_service_connection.value["request_message"]
     }
   }
 
   dynamic "ip_configuration" {
-    
+    for_each = var.ip_configuration
+    content {
+      name = ip_configuration.value["name"]
+      private_ip_address = ip_configuration.value["private_ip_address"]
+      subresource_name = ip_configuration.value["subresource_name"]
+      member_name = ip_configuration.value["member_name"]
+    }
   }
+
+  tags = var.tags
 
 }
---
+++ azurerm_private_endpoint/outputs.tf
@@ -0,0 +1,15 @@
+output "id" {
+  value = azurerm_private_endpoint.this.id
+}
+
+output "network_interface" {
+  value = azurerm_private_endpoint.this.network_interface
+}
+
+output "custom_dns_configs" {
+  value = azurerm_private_endpoint.this.custom_dns_configs
+}
+
+output "name" {
+  
+}
