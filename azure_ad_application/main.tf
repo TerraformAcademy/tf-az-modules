83d3582c11d6886e92aedd9c37e8ea0a2aec1f9f
kotnala <ankitkotnala@live.in>
2023-11-30 13:09:08 +0530

    fmt

--- azure_ad_application/main.tf
+++ azure_ad_application/main.tf
@@ -1,30 +1,30 @@
 resource "azuread_application" "this" {
-  display_name = var.display_name
-  description = var.description
-  device_only_auth_enabled = var.device_only_auth_enabled
+  display_name                   = var.display_name
+  description                    = var.description
+  device_only_auth_enabled       = var.device_only_auth_enabled
   fallback_public_client_enabled = var.fallback_public_client_enabled
-  group_membership_claims = var.group_membership_claims
-  identifier_uris = var.identifier_uris
-  marketing_url = var.marketing_url
-  oauth2_post_response_required = var.oauth2_post_response_required
-  owners = var.owners
-  privacy_statement_url = var.privacy_statement_url
-  sign_in_audience = var.sign_in_audience
-  support_url = var.support_url
-  template_id = var.template_id
-  terms_of_service_url = var.terms_of_service_url
+  group_membership_claims        = var.group_membership_claims
+  identifier_uris                = var.identifier_uris
+  marketing_url                  = var.marketing_url
+  oauth2_post_response_required  = var.oauth2_post_response_required
+  owners                         = var.owners
+  privacy_statement_url          = var.privacy_statement_url
+  sign_in_audience               = var.sign_in_audience
+  support_url                    = var.support_url
+  template_id                    = var.template_id
+  terms_of_service_url           = var.terms_of_service_url
 
   dynamic "web" {
     for_each = var.web
     content {
-      homepage_url = web.value["homepage_url"]
-      logout_url = web.value["logout_url"]
+      homepage_url  = web.value["homepage_url"]
+      logout_url    = web.value["logout_url"]
       redirect_uris = web.value["redirect_uris"]
       dynamic "implicit_grant" {
         for_each = web.value["implicit_grant"]
         content {
           access_token_issuance_enabled = implicit_grant.value["access_token_issuance_enabled"]
-          id_token_issuance_enabled = implicit_grant.value["id_token_issuance_enabled"]
+          id_token_issuance_enabled     = implicit_grant.value["id_token_issuance_enabled"]
         }
       }
     }
@@ -34,11 +34,33 @@ resource "azuread_application" "this" {
     for_each = var.app_role
     content {
       allowed_member_types = app_role.value["allowed_member_types"]
-      description = app_role.value["description"]
-      display_name = app_role.value["display_name"]
-      enabled = app_role.value["enabled"]
-      id = app_role.value["id"]
-      value = app_role.value["value"]
+      description          = app_role.value["description"]
+      display_name         = app_role.value["display_name"]
+      enabled              = app_role.value["enabled"]
+      id                   = app_role.value["id"]
+      value                = app_role.value["value"]
+    }
+  }
+
+  dynamic "api" {
+    for_each = var.api
+    content {
+      known_client_applications      = api.value["known_client_applications"]
+      mapped_claims_enabled          = api.value["mapped_claims_enabled"]
+      requested_access_token_version = api.value["requested_access_token_version"]
+      dynamic "oauth2_permission_scope" {
+        for_each = api.value["oauth2_permission_scope"]
+        content {
+          admin_consent_description  = oauth2_permission_scope.value["admin_consent_description"]
+          admin_consent_display_name = oauth2_permission_scope.value["admin_consent_display_name"]
+          enabled                    = oauth2_permission_scope.value["enabled"]
+          id                         = oauth2_permission_scope.value["id"]
+          type                       = oauth2_permission_scope.value["type"]
+          user_consent_description   = oauth2_permission_scope.value["user_consent_description"]
+          user_consent_display_name  = oauth2_permission_scope.value["user_consent_display_name"]
+          value                      = oauth2_permission_scope.value["value"]
+        }
+      }
     }
   }
 }
--- azure_ad_application/variables.tf
+++ azure_ad_application/variables.tf
@@ -3,90 +3,110 @@ variable "display_name" {
 }
 
 variable "description" {
-  type = string
+  type    = string
   default = null
 }
 
 variable "device_only_auth_enabled" {
-  type = bool
+  type    = bool
   default = false
 }
 
 variable "fallback_public_client_enabled" {
-  type = bool
+  type    = bool
   default = false
 }
 
 variable "group_membership_claims" {
-  type = string
+  type    = list(string)
   default = null
 }
 
 variable "identifier_uris" {
-  type = set(string)
+  type    = list(string)
   default = []
 }
 
- variable "marketing_url" {
-   type = string
-   default = null
- }
+variable "marketing_url" {
+  type    = string
+  default = null
+}
 
- variable "oauth2_post_response_required" {
-   type = bool
-   default = false
- }
+variable "oauth2_post_response_required" {
+  type    = bool
+  default = false
+}
 
- variable "owners" {
-   type = set(string)
-   default = []
- }
+variable "owners" {
+  type    = list(string)
+  default = []
+}
 
- variable "privacy_statement_url" {
-   type = string
-   default = null
- }
+variable "privacy_statement_url" {
+  type    = string
+  default = null
+}
 
- variable "sign_in_audience" {
-   type = string
-   default = null
- }
+variable "sign_in_audience" {
+  type    = string
+  default = null
+}
 
- variable "support_url" {
-   type = string
-   default = null
- }
+variable "support_url" {
+  type    = string
+  default = null
+}
 
- variable "template_id" {
-   type = string
-   default = null
- }
+variable "template_id" {
+  type    = string
+  default = null
+}
 
- variable "terms_of_service_url" {
-   type = string
-   default = null
- }
+variable "terms_of_service_url" {
+  type    = string
+  default = null
+}
 
- variable "web" {
-   type = set(object({
-     homepage_url = optional(string, null)
-     implicit_grant = optional(set(object({
-        access_token_issuance_enabled = optional(bool)
-        id_token_issuance_enabled = optional(bool)
-     })))
-     logout_url = optional(string, null)
-     redirect_uris = optional(set(string), [])
-   }))
- }
+variable "web" {
+  type = set(object({
+    homepage_url = optional(string, null)
+    implicit_grant = optional(set(object({
+      access_token_issuance_enabled = optional(bool)
+      id_token_issuance_enabled     = optional(bool)
+    })), [])
+    logout_url    = optional(string, null)
+    redirect_uris = optional(list(string), [])
+  }))
+  default = []
+}
 
- variable "app_role" {
-    type = set(object({
-      allowed_member_types = set(string)
-      description = string
-      display_name = string
-      enabled = optional(bool, true)
-      id = string
-      value = optional(string, null)
-    }))
-   
- }
+variable "app_role" {
+  type = set(object({
+    allowed_member_types = list(string)
+    description          = string
+    display_name         = string
+    enabled              = optional(bool, true)
+    id                   = string
+    value                = optional(string, null)
+  }))
+  default = []
+}
+
+variable "api" {
+  type = set(object({
+    known_client_applications      = optional(list(string), [])
+    mapped_claims_enabled          = optional(bool, false)
+    requested_access_token_version = optional(string, null)
+    oauth2_permission_scope = optional(set(object({
+      admin_consent_description  = string
+      admin_consent_display_name = string
+      enabled                    = optional(bool, true)
+      id                         = string
+      type                       = optional(string, "user")
+      user_consent_description   = optional(string, null)
+      user_consent_display_name  = optional(string, null)
+      value                      = optional(string, null)
+    })), [])
+  }))
+  default = []
+}
