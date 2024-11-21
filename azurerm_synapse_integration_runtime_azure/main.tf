ab3e064ff8a7c6cf7ddc127494cc032efd81ffb0
Ankit Kotnala <31920677+ankitkotnala@users.noreply.github.com>
2024-11-21 01:53:08 -0600

    Update main.tf

--- azurerm_synapse_integration_runtime_azure/main.tf
+++ azurerm_synapse_integration_runtime_azure/main.tf
@@ -2,4 +2,9 @@ resource "azurerm_synapse_integration_runtime_azure" "example" {
   name                 = var.name
   synapse_workspace_id = var.synapse_workspace_id
   location             = var.location
+  compute_type         = var.compute_type
+  core_count           = var.core_count
+  description          = var.description
+  time_to_live_min     = var.time_to_live_min
 }
+ 
