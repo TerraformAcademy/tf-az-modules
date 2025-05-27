781a357c2093e0516396b6104e6f83245d0d0cbd
Ankit Kotnala <31920677+ankitkotnala@users.noreply.github.com>
2025-05-27 13:59:08 +0530

    Update main.tf

--- azurerm_application_gateway/main.tf
+++ azurerm_application_gateway/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.40, <4.00"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
 resource "azurerm_application_gateway" "this" {
   name                = var.name
   resource_group_name = var.resource_group_name
@@ -27,13 +37,21 @@ resource "azurerm_application_gateway" "this" {
       pick_host_name_from_backend_address = backend_http_settings.value["pick_host_name_from_backend_address"]
 
       dynamic "authentication_certificate" {
-        for_each = backend_http_settings.value["authentication_certificate"]
+        for_each = backend_http_settings.value["authentication_certificate_backend"]
         content {
           name = authentication_certificate.value["name"]
         }
       }
 
       trusted_root_certificate_names = backend_http_settings.value["trusted_root_certificate_names"]
+
+      dynamic "connection_draining" {
+        for_each = backend_http_settings.value["connection_draining"]
+        content {
+          enabled           = connection_draining.value["enabled"]
+          drain_timeout_sec = connection_draining.value["drain_timeout_sec"]
+        }
+      }
     }
   }
 
@@ -85,7 +103,7 @@ resource "azurerm_application_gateway" "this" {
         }
       }
 
-      firewall_policy_id = http_listener.value["firewall_policy_id"]
+      firewall_policy_id = var.create_waf ? azurerm_web_application_firewall_policy.this[0].id : null #http_listener.value["firewall_policy_id"]
       ssl_profile_name   = http_listener.value["ssl_profile_name"]
     }
 
@@ -98,6 +116,7 @@ resource "azurerm_application_gateway" "this" {
       rule_type                   = request_routing_rule.value["rule_type"]
       http_listener_name          = request_routing_rule.value["http_listener_name"]
       backend_address_pool_name   = request_routing_rule.value["backend_address_pool_name"]
+      backend_http_settings_name  = request_routing_rule.value["backend_http_settings_name"]
       redirect_configuration_name = request_routing_rule.value["redirect_configuration_name"]
       rewrite_rule_set_name       = request_routing_rule.value["rewrite_rule_set_name"]
       url_path_map_name           = request_routing_rule.value["url_path_map_name"]
@@ -112,6 +131,7 @@ resource "azurerm_application_gateway" "this" {
   }
 
   fips_enabled = var.fips_enabled
+
   dynamic "global" {
     for_each = var.global
     content {
@@ -257,7 +277,7 @@ resource "azurerm_application_gateway" "this" {
           backend_http_settings_name  = path_rule.value["backend_http_settings_name"]
           redirect_configuration_name = path_rule.value["redirect_configuration_name"]
           rewrite_rule_set_name       = path_rule.value["rewrite_rule_set_name"]
-          firewall_policy_id          = path_rule.value["firewall_policy_id"]
+          firewall_policy_id          = var.create_waf ? azurerm_web_application_firewall_policy.this[0].id : null #path_rule.value["firewall_policy_id"]
         }
       }
     }
@@ -300,7 +320,7 @@ resource "azurerm_application_gateway" "this" {
     }
   }
 
-  firewall_policy_id = var.firewall_policy_id
+  firewall_policy_id = var.create_waf ? azurerm_web_application_firewall_policy.this[0].id : null
   dynamic "redirect_configuration" {
     for_each = var.redirect_configuration
     content {
@@ -369,5 +389,121 @@ resource "azurerm_application_gateway" "this" {
       }
     }
   }
+}
 
+resource "azurerm_web_application_firewall_policy" "this" {
+  count               = var.create_waf ? 1 : 0
+  name                = var.waf_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  dynamic "custom_rules" {
+    for_each = var.custom_rules
+    content {
+      enabled   = custom_rules.value["enabled"]
+      name      = custom_rules.value["name"]
+      priority  = custom_rules.value["priority"]
+      rule_type = custom_rules.value["rule-type"]
+
+      dynamic "match_conditions" {
+        for_each = custom_rules.value["match_condition"]
+        content {
+          dynamic "match_variables" {
+            for_each = match_conditions.value["match_variables"]
+            content {
+              variable_name = match_variables.value["variable_name"]
+              selector      = match_variables.value["selector"]
+            }
+          }
+          match_values       = match_conditions.value["match_values"]
+          operator           = match_conditions.value["operator"]
+          negation_condition = match_conditions.value["negation_condition"]
+          transforms         = match_conditions.value["transforms"]
+        }
+      }
+
+      action               = custom_rules.value["action"]
+      rate_limit_duration  = custom_rules.value["rate_limit_duration"]
+      rate_limit_threshold = custom_rules.value["rate_limit_threshold"]
+      group_rate_limit_by  = custom_rules.value["group_rate_limit_by"]
+    }
+  }
+
+  dynamic "policy_settings" {
+    for_each = var.policy_settings
+    content {
+      enabled                     = policy_settings.value["enabled"]
+      mode                        = policy_settings.value["mode"]
+      file_upload_limit_in_mb     = policy_settings.value["file_upload_limit_in_mb"]
+      request_body_check          = policy_settings.value["request_body_check"]
+      max_request_body_size_in_kb = policy_settings.value["max_request_body_size_in_kb"]
+      dynamic "log_scrubbing" {
+        for_each = policy_settings.value["log_scrubbing"]
+        content {
+          enabled = log_scrubbing.value["enabled"]
+          dynamic "rule" {
+            for_each = log_scrubbing.value["rule"]
+            content {
+              enabled                 = rule.value["enabled"]
+              match_variable          = rule.value["match_variable"]
+              selector_match_operator = rule.value["selector_match_operator"]
+              selector                = rule.value["selector"]
+            }
+          }
+        }
+      }
+      request_body_inspect_limit_in_kb = policy_settings.value["request_body_inspect_limit_in_kb"]
+    }
+  }
+
+  dynamic "managed_rules" {
+    for_each = var.managed_rules
+    content {
+      dynamic "exclusion" {
+        for_each = managed_rules.value["exclusion"]
+        content {
+          match_variable          = exclusion.value["match_variable"]
+          selector                = exclusion.value["selector"]
+          selector_match_operator = exclusion.value["selector_match_operator"]
+          dynamic "excluded_rule_set" {
+            for_each = exclusion.value["excluded_rule_set"]
+            content {
+              type    = excluded_rule_set.value["type"]
+              version = excluded_rule_set.value["version"]
+              dynamic "rule_group" {
+                for_each = excluded_rule_set.value["rule_group"]
+                content {
+                  rule_group_name = rule_group.value["rule_group_name"]
+                  excluded_rules  = rule_group.value["excluded_rules"]
+                }
+              }
+            }
+          }
+        }
+      }
+
+      dynamic "managed_rule_set" {
+        for_each = managed_rules.value["managed_rule_set"]
+        content {
+          type    = managed_rule_set.value["type"]
+          version = managed_rule_set.value["version"]
+          dynamic "rule_group_override" {
+            for_each = managed_rule_set.value["rule_group_override"]
+            content {
+              rule_group_name = rule_group_override.value["rule_group_name"]
+              dynamic "rule" {
+                for_each = rule_group_override.value["rule"]
+                content {
+                  id      = rule.value["id"]
+                  enabled = rule.value["enabled"]
+                  action  = rule.value["action"]
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  tags = var.tags
 }
