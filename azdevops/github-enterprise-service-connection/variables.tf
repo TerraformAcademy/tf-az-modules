c73d4b15f54848cf113f345195188558f3335507
Ankit Kotnala <31920677+ankitkotnala@users.noreply.github.com>
2025-09-22 14:47:29 +0530

    Update variables.tf

--- azdevops/github-enterprise-service-connection/variables.tf
+++ azdevops/github-enterprise-service-connection/variables.tf
@@ -6,6 +6,17 @@ variable "service_endpoint_name"{
   type = string
 }
 
+variable "url"{
+  type = string
+  default = null
+}
+
+variable "description"{
+  type = string
+  default = null
+}
+
+
 variable "personal_access_token"{
   type = string
 }
